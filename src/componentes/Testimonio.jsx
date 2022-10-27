4a886098f25c0e0043b39539a2bfc80040c78b8a
bioodev <bioodev@protonmail.com>
2022-10-27 13:20:57 -0300

    mensaje

---
+++ src/componentes/Testimonio.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import '../hojas-de-estilo/Testimonio.css';
+
+function Testimonio(props) {
+    return (
+        <div className="contenedor-testimonio">
+            <img src={require(`../imagenes/t-${props.imagen}.png`)} alt={props.nombre} />
+            <div className="contenedor-texto-testimonio">
+            <p className="nombre-testimonio">
+                <strong>{props.nombre}</strong> en {props.pais}
+            </p>
+            <p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong></p>
+            <p className="texto-testimonio">"{props.testimonio}"</p>
+            </div>
+        </div>
+    );
+};
+
+export default Testimonio;
